43f00eee35c945f530a7b3380e86fd3aa7af3e1c
kong0706 <89024259+kong0706@users.noreply.github.com>
2025-10-24 10:25:05 +0800

    Add files via upload

--- gromacs-workflow/script/md_preparation.sh
+++ gromacs-workflow/script/md_preparation.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # 受体制备
-gmx_mpi pdb2gmx -f pro.pdb -o protein.gro -water tip3p -ignh <<< 1 #自行选择力场编号
+gmx_mpi pdb2gmx -f pro.pdb -o protein.gro -water tip3p -ignh <<< 1 #自行选择力场编号，AMBER14SB是我后来新增的力场，不包括在gromacs自带的力场文件中。
 
 # 配体制备
-#将配体名称改为MOL，不需要再改mdp文件了
+# md.mdp文件将受体和配体定义为一个组，配体的命名为MOL，在配体制备前期先将配体名称改为MOL，后续就不需要再改md.mdp文件了。
 awk '
 BEGIN { in_atom=0 }
 /@<TRIPOS>ATOM/ { in_atom=1; print; next }
@@ -17,17 +17,19 @@ BEGIN { in_atom=0 }
 }
 ' lig.mol2 > tmp.mol2 && mv tmp.mol2 lig.mol2
 
-antechamber -i lig.mol2 -fi mol2 -o ligand_bcc.mol2 -fo mol2 -at gaff -c bcc -nc 0 -pf y || { echo "Failed to run command  at line ${LINENO} in ${BASH_SOURCE}" && exit 1; } #根据净电荷自行修改-nc参数
+# 4个步骤分别是拟合电荷、生成Amber力场参数文件、构建Amber拓扑与坐标文件、转换为 GROMACS 格式文件
+antechamber -i lig.mol2 -fi mol2 -o ligand_bcc.mol2 -fo mol2 -at gaff -c bcc -nc 0 -pf y || { echo "Failed to run command  at line ${LINENO} in ${BASH_SOURCE}" && exit 1; } #根据原子电荷自行修改-nc参数
 parmchk2 -i ligand_bcc.mol2 -f mol2 -o ligand.frcmod || { echo "Failed to run command  at line ${LINENO} in ${BASH_SOURCE}" && exit 1; }
 tleap -f ../script/tleap_ligand.in || { echo "Failed to run command  at line ${LINENO} in ${BASH_SOURCE}" && exit 1; }
 python3 ../script/pmed_amb2gmx.py -p ligand.prmtop -x ligand.inpcrd -o ligand || { echo "Failed to run command  at line ${LINENO} in ${BASH_SOURCE}" && exit 1; }
 
+#删除system、molecules以及defaults行及其后面的两行内容
 cp ligand.top ligand.itp
 sed -i '/system/,+2 d' ligand.itp
 sed -i '/molecules/,+2 d' ligand.itp
 sed -i '/defaults/,+2 d' ligand.itp
 
-# 限制
+# 给配体加上位置限制势能
 gmx_mpi make_ndx -f ligand.gro -o index.ndx << INPUT
 2 & ! a H*
 q
@@ -35,7 +37,7 @@ INPUT
 gmx_mpi genrestr -f ligand.gro -o posre_ligand.itp -n index.ndx -fc 1000 1000 1000 <<< 3 || { echo "Failed to run command  at line ${LINENO} in ${BASH_SOURCE}" && exit 1; }
 
 # 复合物制备
-#合并gro文件
+# merge_gro.py脚本可实现合并gro文件，并自动修改原子数
 python ../script/merge_gro.py
 
 # 检查 ligand.itp 文件末尾是否已经有 #include "posre_ligand.itp"
